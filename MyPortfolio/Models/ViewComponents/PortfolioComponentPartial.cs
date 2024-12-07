312d1404e8120f326fe914b1db78e1738f6ea794
Abdullah Şentürk <asenturk22@yandex.com>
2024-12-08 00:14:43 +0300

    Experience ve Portfolio Components eklendi

---
+++ MyPortfolio/Models/ViewComponents/PortfolioComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolio.Models.ViewComponents
+{
+    public class PortfolioComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
