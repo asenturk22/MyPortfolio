bec7d02ec7427b3b6ec062ad9aa85765beb150c1
Abdullah Şentürk <asenturk22@yandex.com>
2024-12-15 23:51:51 +0300

    admin panel de deneyimler bölümü oluşturuldu

--- MyPortfolio/Controllers/ExperienceController.cs
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult ExperienceList()
         {
-            return View();
+            var values = context.Experiences.ToList();
+
+            return View(values);
         }
     }
 }
