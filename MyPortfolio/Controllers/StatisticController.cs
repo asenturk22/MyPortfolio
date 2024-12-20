0bc775ae35e2e1e2198a8be62ab2a5147d8b27c8
Abdullah Şentürk <asenturk22@yandex.com>
2024-12-21 00:09:48 +0300

    Statistic bölümü eklendi.

---
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        MyPortfolioContext m_context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            ViewBag.v1 = m_context.Skills.Count();
+            ViewBag.v2 = m_context.Messages.Count();
+            ViewBag.v3 = m_context.Messages.Where(x => x.IsRead == false).Count();
+            ViewBag.v4 = m_context.Messages.Where(x => x.IsRead == true).Count();
+            return View();
+        }
+    }
+}
