7431b4436ecb6cdec42c57d4f637a01d5222f15e
Abdullah Şentürk <asenturk22@yandex.com>
2024-12-20 23:41:20 +0300

    Message alanı oluşturuldu.

---
+++ MyPortfolio/Controllers/MessageController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolioContext m_context = new MyPortfolioContext();
+        public IActionResult Inbox()
+        {
+            var values = m_context.Messages.ToList();
+
+            return View(values);
+        }
+
+        public IActionResult ChangeIsReadToTrue(int id)
+        {
+            var value = m_context.Messages.Find(id);
+            value.IsRead = true;
+            m_context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult ChangeIsReadToFalse(int id)
+        {
+            var value = m_context.Messages.Find(id);
+            value.IsRead = false;
+            m_context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = m_context.Messages.Find(id);
+            m_context.Messages.Remove(value);
+            m_context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult MessageDetail(int id)
+        {
+            var value = m_context.Messages.Find(id);
+            return View(value);
+        }
+    }
+}
--- MyPortfolio/DAL/Entities/Message.cs
+++ MyPortfolio/DAL/Entities/Message.cs
@@ -6,6 +6,7 @@
         public string NameSurName { get; set; }
         public string Subject { get; set; }
         public string MessageDetail { get; set; }
+        public string Email { get; set; }
         public DateTime SendDate { get; set; }
         public bool IsRead { get; set; }
 
